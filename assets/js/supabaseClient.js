112a209e7223e9e6645ca5ede735329cd5abcfd1
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 11:31:57 -0400

    Update Supabase client to use new CDN and key

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,12 +1,12 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.8/dist/supabase.min.js'
 
 const SUPABASE_URL = "https://pnnnhkbriqizjmkhodbu.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubm5oa2JyaXFpempta2hvZGJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNjU3MzEsImV4cCI6MjEwNTc0MTczMX0.IlDYR2qUoXAHggBm_Sy7lPVgNOVxxXLhzQltTaXwAHE"
+const SUPABASE_ANON_KEY = "sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx"
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 if (supabaseClient) {
-  console.log('✅ Supabase inicializado correctamente con la nueva base de datos.');
+  console.log('✅ Supabase inicializado correctamente con la Publishable Key.');
 } else {
   console.error('❌ Error al inicializar Supabase.');
 }

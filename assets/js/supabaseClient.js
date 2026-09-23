9a566196a48845e8d2669105fe9cb7721837330a
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 19:38:07 -0400

    Actualizar supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,11 +1,7 @@
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL =
-  'https://pnnnhkbriqizjmkhodbu.supabase.co'
-
-const SUPABASE_PUBLISHABLE_KEY =
-  'sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx'
+const SUPABASE_URL = 'https://pnnnhkbriqizjmkhodbu.supabase.co'
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx'
 
 export const supabaseClient = createClient(
   SUPABASE_URL,

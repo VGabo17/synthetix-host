8a0c649ada3479aaf40592a690dac73188117667
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 19:16:06 -0400

    Actualizar supabaseClient.js

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,12 +1,19 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.39.8/dist/supabase.min.js'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = "https://pnnnhkbriqizjmkhodbu.supabase.co"
-const SUPABASE_ANON_KEY = "COPIA_LA_KEY_EXACTA_DE_TU_PROJECT_SUPABASE"
+const SUPABASE_URL = 'https://pnnnhkbriqizjmkhodbu.supabase.co'
+const SUPABASE_PUBLISHABLE_KEY =
+  'sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx'
 
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true
+export const supabaseClient = createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
   }
-})
+)
+
+console.log('✅ Supabase conectado correctamente')

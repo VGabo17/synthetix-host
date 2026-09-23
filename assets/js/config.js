9c05f06010b9c3b140c9a3d733101a80e1f61f0d
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 19:16:46 -0400

    Actualizar config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,11 +1,16 @@
 const CONFIG = {
-  SUPABASE_URL: "https://pnnnhkbriqizjmkhodbu.supabase.co",
-  SUPABASE_ANON_KEY: "COPIA_LA_KEY_EXACTA_DE_TU_PROJECT_SUPABASE",
-  PAYMENTER_URL: "https://billing.tudominio.com",
-  PTERODACTYL_URL: "https://panel.tudominio.com",
-  DISCORD_INVITE_URL: "https://discord.gg/tudominio",
-  DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/tu-webhook-id/tu-webhook-token",
-  BRAND_NAME: "Synthetix Host",
-  CURRENCY: "$",
-  SUPPORT_EMAIL: "soporte@tudominio.com"
-};
+  SUPABASE_URL: 'https://pnnnhkbriqizjmkhodbu.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx',
+
+  PAYMENTER_URL: 'https://billing.tudominio.com',
+  PTERODACTYL_URL: 'https://panel.tudominio.com',
+  DISCORD_INVITE_URL: 'https://discord.gg/tudominio',
+  DISCORD_WEBHOOK_URL:
+    'https://discord.com/api/webhooks/tu-webhook-id/tu-webhook-token',
+
+  BRAND_NAME: 'Synthetix Host',
+  CURRENCY: '$',
+  SUPPORT_EMAIL: 'soporte@tudominio.com'
+}
+
+export default CONFIG

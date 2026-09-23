069143c0c9defb341fc3de038956980736dac306
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 11:29:35 -0400

    Change Supabase anonymous key
    Updated the Supabase anonymous key to a new value.

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,8 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-// Asegúrate de importar o tener disponible CONFIG en este archivo, o define las credenciales directo:
 const SUPABASE_URL = "https://pnnnhkbriqizjmkhodbu.supabase.co"
-const SUPABASE_ANON_KEY = "Sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBubm5oa2JyaXFpempta2hvZGJ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxNjU3MzEsImV4cCI6MjEwNTc0MTczMX0.IlDYR2qUoXAHggBm_Sy7lPVgNOVxxXLhzQltTaXwAHE"
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 

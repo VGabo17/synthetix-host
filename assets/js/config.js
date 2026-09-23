11f3298d78febc971b13b9bc52f276025ed8e12f
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-23 18:14:50 -0400

    Actualizar config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,17 +1,11 @@
-// Configuración Global del Proyecto
 const CONFIG = {
-  // Credenciales de Supabase (Actualizadas a tu nueva base de datos)
   SUPABASE_URL: "https://pnnnhkbriqizjmkhodbu.supabase.co",
-  SUPABASE_ANON_KEY: "Sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx",
-
-  // URLs de Infraestructura Externa
+  SUPABASE_ANON_KEY: "COPIA_LA_KEY_EXACTA_DE_TU_PROJECT_SUPABASE",
   PAYMENTER_URL: "https://billing.tudominio.com",
   PTERODACTYL_URL: "https://panel.tudominio.com",
   DISCORD_INVITE_URL: "https://discord.gg/tudominio",
   DISCORD_WEBHOOK_URL: "https://discord.com/api/webhooks/tu-webhook-id/tu-webhook-token",
-
-  // Datos de la Marca
-  BRAND_NAME: "Synthetix Host", // O NixerHost, según prefieras
+  BRAND_NAME: "Synthetix Host",
   CURRENCY: "$",
   SUPPORT_EMAIL: "soporte@tudominio.com"
 };

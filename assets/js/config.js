20d12122344c11db448a903ddb40f9a0df36b584
VGabo17 <162368407+VGabo17@users.noreply.github.com>
2026-09-26 00:49:47 -0400

    Update PTERODACTYL_URL to new domain

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   SUPABASE_ANON_KEY: 'sb_publishable_6mgwm2JwyzzveWGAGCXyJQ_NjF1Mxrx',
 
   PAYMENTER_URL: 'https://billing.tudominio.com',
-  PTERODACTYL_URL: 'https://panel.tudominio.com',
+  PTERODACTYL_URL: 'https://panel.nixermc.lol',
   DISCORD_INVITE_URL: 'https://discord.gg/tudominio',
   DISCORD_WEBHOOK_URL:
     'https://discord.com/api/webhooks/tu-webhook-id/tu-webhook-token',
